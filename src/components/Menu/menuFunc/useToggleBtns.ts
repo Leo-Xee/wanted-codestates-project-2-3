70e55e3194078df7b6ce7e2b2588d8afc35be311
jinn2u <bsybear623@gmail.com>
2022-03-16 20:11:12 +0900

    feat: useToggleBtns 훅

--- src/components/Menu/menuFunc/useToggleBtns.ts
+++ src/components/Menu/menuFunc/useToggleBtns.ts
@@ -1,9 +1,15 @@
-import { useReducer } from "react";
-import { initialSetting, settingReducer, SettingType } from "../settingReducer";
-import { Action, Setting } from "../settingReducer/types";
+import { ChangeEvent, MouseEvent } from "react";
+import { SettingType } from "../settingReducer";
+import { Action, ItemSize, Setting } from "../settingReducer/types";
+import { UseToggleBtns } from "./types";
+import { canChangeNumber } from "./canChangeNumber";
 
-export const useToggleBtns = (settingState: Setting, dispatch: React.Dispatch<Action>) => {
+export const useToggleBtns = (
+  settingState: Setting,
+  dispatch: React.Dispatch<Action>,
+): UseToggleBtns => {
   const { showTitle, showSearch, multiSelect, showSelectedItem } = settingState;
+
   const handleToggleShowTitle = () => {
     dispatch({ type: SettingType.TOGGLE_SHOW_TITLE, showTitle: !showTitle });
   };
@@ -19,10 +25,47 @@ export const useToggleBtns = (settingState: Setting, dispatch: React.Dispatch<Ac
       showSelectedItem: !showSelectedItem,
     });
   };
+  const handleChangeAvailableTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    const title = e.target.value;
+    dispatch({
+      type: SettingType.SET_AVAILABLE_TITLE,
+      availableTitle: title,
+    });
+  };
+  const handleChangeSelectedTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    const title = e.target.value;
+    dispatch({
+      type: SettingType.SET_SELECTED_TITLE,
+      selectedTitle: title,
+    });
+  };
+  const handleUlClick = (e: MouseEvent<HTMLUListElement>) => {
+    const target = e.target as HTMLElement;
+    if (target.tagName !== "INPUT") return;
+    const size = (target as HTMLInputElement).value as ItemSize;
+    dispatch({ type: "SET_ITEM_SIZE", itemSize: size });
+  };
+  const handleChangeWidth = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    if (!canChangeNumber(value)) return;
+    const width = +value;
+    dispatch({ type: "SET_WIDTH", width });
+  };
+  const handleChangeHeight = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    if (!canChangeNumber(value)) return;
+    const height = +value;
+    dispatch({ type: "SET_HEIGHT", height });
+  };
   return [
     handleToggleShowTitle,
     handleToggleShowSearch,
     handleToggleMultiSelect,
     handleToggleShowSelectedItem,
+    handleChangeAvailableTitle,
+    handleChangeSelectedTitle,
+    handleUlClick,
+    handleChangeWidth,
+    handleChangeHeight,
   ];
 };
