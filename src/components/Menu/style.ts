5e0142c042705cf12e78216e39588a9dba44ceb5
jinn2u <bsybear623@gmail.com>
2022-03-16 17:14:10 +0900

    feat: 상태에 따라 버튼의 색이 바뀌게 변경

--- src/components/Menu/style.ts
+++ src/components/Menu/style.ts
@@ -4,11 +4,11 @@ export const MenuButton = styled.button`
   position: fixed;
   top: 20px;
   right: 20px;
+  padding: 20px;
+  font-size: 2rem;
   background: white;
   border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  font-size: 2rem;
-  padding: 20px;
+  box-shadow: 0 0 10px rgb(0 0 0 / 20%);
 `;
 
 export const MenuContainer = styled.div`
@@ -16,18 +16,20 @@ export const MenuContainer = styled.div`
   top: 95px;
   right: 20px;
   width: 200px;
+  overflow: hidden;
   background: white;
   border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  overflow: hidden;
+  box-shadow: 0 0 10px rgb(0 0 0 / 20%);
 `;
 
-export const MenuItem = styled.div`
-  background: white;
+export const ToggleInput = styled.div`
+  display: flex;
+  justify-content: space-between;
   padding: 15px 20px;
   font-size: 1.6rem;
+  background: white;
 
   & + & {
-    border-top: 1px solid rgba(0, 0, 0, 0.1);
+    border-top: 1px solid rgb(0 0 0 / 10%);
   }
 `;
