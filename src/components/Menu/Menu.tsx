eb252036c9f9ebe68b01cdb3b53dff0a0b1495b8
jinn2u <bsybear623@gmail.com>
2022-03-16 21:40:31 +0900

    feat: 환경설정 마크업

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,24 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useReducer, useState } from "react";
 import * as S from "./style";
 
 import { BiMenu } from "react-icons/bi";
-import { settingReducer, initialSetting, SettingType } from "./settingReducer";
+import { settingReducer, initialSetting } from "./settingReducer";
 import { useToggleBtns } from "./menuFunc/useToggleBtns";
 
 function Menu() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [settingState, dispatch] = useReducer(settingReducer, initialSetting);
-  const { showTitle, showSearch, multiSelect, showSelectedItem } = settingState;
+  const {
+    showTitle,
+    showSearch,
+    multiSelect,
+    showSelectedItem,
+    availableTitle,
+    selectedTitle,
+    itemSize,
+    width,
+    height,
+  } = settingState;
 
   const handleToggleMenu = () => {
     setMenuOpen((prev) => !prev);
@@ -18,7 +28,13 @@ function Menu() {
     handleToggleShowSearch,
     handleToggleMultiSelect,
     handleToggleShowSelectedItem,
+    handleChangeAvailableTitle,
+    handleChangeSelectedTitle,
+    handleUlClick,
+    handleChangeWidth,
+    handleChangeHeight,
   ] = useToggleBtns(settingState, dispatch);
+
   return (
     <>
       <S.MenuButton onClick={handleToggleMenu}>
@@ -26,23 +42,63 @@ function Menu() {
       </S.MenuButton>
       {menuOpen && (
         <S.MenuContainer>
-          <S.ToggleInput>
-            <span>타이틀</span>{" "}
+          <S.ToggleDiv>
+            <span>타이틀</span>
             <span onClick={handleToggleShowTitle}>{showTitle ? "🟢 " : "🔴"} </span>
-          </S.ToggleInput>
-          <S.ToggleInput>
+          </S.ToggleDiv>
+          <S.InpDiv>
+            <S.Input placeholder={availableTitle} onChange={handleChangeAvailableTitle} />
+            <S.Input placeholder={selectedTitle} onChange={handleChangeSelectedTitle} />
+          </S.InpDiv>
+          <S.ToggleDiv>
             <span>검색</span>
             <span onClick={handleToggleShowSearch}>{showSearch ? "🟢 " : "🔴"} </span>
-          </S.ToggleInput>
-          <S.ToggleInput>
-            {" "}
+          </S.ToggleDiv>
+          <S.ToggleDiv>
             <span>하나씩만 옮기기</span>
             <span onClick={handleToggleMultiSelect}>{multiSelect ? "🟢 " : "🔴"} </span>
-          </S.ToggleInput>
-          <S.ToggleInput>
+          </S.ToggleDiv>
+          <S.ToggleDiv>
             <span>선택된 아이템 갯수 표시</span>
             <span onClick={handleToggleShowSelectedItem}>{showSelectedItem ? "🟢 " : "🔴"} </span>
-          </S.ToggleInput>
+          </S.ToggleDiv>
+          <S.Ul onClick={handleUlClick}>
+            <li>아이템 크기</li>
+            <S.Li>
+              <S.Input
+                type="radio"
+                id="item-size-xs"
+                name="item-size"
+                value="xs"
+                checked={itemSize === "xs"}
+              />
+              <label htmlFor="item-size-xs">xs</label>
+            </S.Li>
+            <S.Li>
+              <S.Input
+                type="radio"
+                id="item-size-s"
+                name="item-size"
+                value="s"
+                checked={itemSize === "s"}
+              />
+              <label htmlFor="item-size-s">s</label>
+            </S.Li>
+            <S.Li>
+              <S.Input
+                type="radio"
+                id="item-size-m"
+                name="item-size"
+                value="m"
+                checked={itemSize === "m"}
+              />
+              <label htmlFor="item-size-m">m</label>
+            </S.Li>
+          </S.Ul>
+          <S.InpDiv>
+            <S.Input placeholder={`가로 (현재: ${width} px)`} onChange={handleChangeWidth} />
+            <S.Input placeholder={`가로 (현재: ${height} px)`} onChange={handleChangeHeight} />
+          </S.InpDiv>
         </S.MenuContainer>
       )}
     </>
--- src/components/Menu/style.ts
+++ src/components/Menu/style.ts
@@ -4,7 +4,6 @@ export const MenuButton = styled.button`
   position: fixed;
   top: 20px;
   right: 20px;
-  padding: 20px;
   font-size: 2rem;
   background: white;
   border-radius: 10px;
@@ -15,21 +14,49 @@ export const MenuContainer = styled.div`
   position: fixed;
   top: 95px;
   right: 20px;
-  width: 200px;
+  box-sizing: border-box;
+  width: 250px;
   overflow: hidden;
+  font-size: 1.6rem;
   background: white;
   border-radius: 10px;
   box-shadow: 0 0 10px rgb(0 0 0 / 20%);
+
+  & > * {
+    padding: 15px 20px;
+  }
+
+  & > *:not(:last-child) {
+    border-bottom: 1px solid rgb(0 0 0 / 10%);
+  }
 `;
 
-export const ToggleInput = styled.div`
+export const ToggleDiv = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 15px 20px;
-  font-size: 1.6rem;
   background: white;
-
-  & + & {
-    border-top: 1px solid rgb(0 0 0 / 10%);
-  }
+`;
+export const InpDiv = styled.div`
+  justify-content: space-between;
+  background: white;
+`;
+export const Input = styled.input`
+  width: 100%;
+  height: 4rem;
+  padding: 1rem;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  outline: none;
+`;
+export const Ul = styled.ul`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  list-style: none;
+`;
+export const Li = styled.li`
+  display: flex;
+  align-items: flex-end;
+  width: 1.6rem;
+  margin-right: 2rem;
 `;
