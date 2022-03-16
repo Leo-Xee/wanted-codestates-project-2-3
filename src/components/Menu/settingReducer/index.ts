8df1c980c2363454fde64ab8d8feaa99beb5c660
jinn2u <bsybear623@gmail.com>
2022-03-16 21:38:34 +0900

    feat: settingReducer 작성

--- src/components/Menu/settingReducer/index.ts
+++ src/components/Menu/settingReducer/index.ts
@@ -17,12 +17,16 @@ export const SettingType = {
   TOGGLE_SHOW_SEARCH: "TOGGLE_SHOW_SEARCH",
   TOGGLE_MULTI_SELECT: "TOGGLE_MULTI_SELECT",
   TOGGLE_SHOW_SELECTED_ITEM: "TOGGLE_SHOW_SELECTED_ITEM",
+  SET_AVAILABLE_TITLE: "SET_AVAILABLE_TITLE",
+  SET_SELECTED_TITLE: "SET_SELECTED_TITLE",
+  SET_ITEM_SIZE: "SET_ITEM_SIZE",
+  SET_WIDTH: "SET_WIDTH",
+  SET_HEIGHT: "SET_HEIGHT",
 } as const;
 
 export const settingReducer = (state: Setting, action: Action): Setting => {
   switch (action.type) {
     case SettingType.TOGGLE_SHOW_TITLE: {
-      console.log(action.showTitle, "reducer");
       return { ...state, showTitle: action.showTitle };
     }
     case SettingType.TOGGLE_SHOW_SEARCH: {
@@ -34,6 +38,22 @@ export const settingReducer = (state: Setting, action: Action): Setting => {
     case SettingType.TOGGLE_SHOW_SELECTED_ITEM: {
       return { ...state, showSelectedItem: action.showSelectedItem };
     }
+    case SettingType.SET_AVAILABLE_TITLE: {
+      return { ...state, availableTitle: action.availableTitle };
+    }
+    case SettingType.SET_SELECTED_TITLE: {
+      return { ...state, selectedTitle: action.selectedTitle };
+    }
+    case SettingType.SET_ITEM_SIZE: {
+      return { ...state, itemSize: action.itemSize };
+    }
+    case SettingType.SET_WIDTH: {
+      return { ...state, width: action.width };
+    }
+    case SettingType.SET_HEIGHT: {
+      return { ...state, height: action.height };
+    }
+
     default:
       throw new Error("Unhandled action");
   }
