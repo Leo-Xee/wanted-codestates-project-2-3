3f5168c201093278c118db0a61ea8f6f26581e7f
jinn2u <bsybear623@gmail.com>
2022-03-16 21:08:38 +0900

    feat: settingreducer type 정의

--- src/components/Menu/settingReducer/types.ts
+++ src/components/Menu/settingReducer/types.ts
@@ -16,4 +16,9 @@ export type Action =
   | { type: "TOGGLE_SHOW_TITLE"; showTitle: boolean }
   | { type: "TOGGLE_SHOW_SEARCH"; showSearch: boolean }
   | { type: "TOGGLE_MULTI_SELECT"; multiSelect: boolean }
-  | { type: "TOGGLE_SHOW_SELECTED_ITEM"; showSelectedItem: boolean };
+  | { type: "TOGGLE_SHOW_SELECTED_ITEM"; showSelectedItem: boolean }
+  | { type: "SET_AVAILABLE_TITLE"; availableTitle: string }
+  | { type: "SET_SELECTED_TITLE"; selectedTitle: string }
+  | { type: "SET_ITEM_SIZE"; itemSize: ItemSize }
+  | { type: "SET_WIDTH"; width: number }
+  | { type: "SET_HEIGHT"; height: number };
