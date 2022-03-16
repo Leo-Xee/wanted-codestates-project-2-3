c72dc2c8f76651380f75e1b158af051720bc6eaf
Kae <kaehehehe@gmail.com>
2022-03-16 21:54:05 +0900

    feat: dnd 구현

--- src/components/DualSelector/DualSelector.tsx
+++ src/components/DualSelector/DualSelector.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 
 import SearchBar from "../SearchBar";
 import Selector from "../Selector";
 import ControlButtons from "../ControlButtons";
+import { emojiMenus, EmojiMenu } from "../../data/emojiMenus";
 
 function DualSelector() {
+  const [menus, setMenus] = useState<EmojiMenu[] | []>(emojiMenus);
+  const [selectedMenus, setSelectedMenus] = useState<EmojiMenu[] | []>([]);
+
   return (
     <S.DualSelectorContainer>
       <S.InputWithSelectorContainer>
         <SearchBar />
-        <Selector />
+        <Selector data={menus} setData={setMenus} />
       </S.InputWithSelectorContainer>
       <ControlButtons />
       <S.InputWithSelectorContainer>
         <SearchBar />
-        <Selector />
+        <Selector data={selectedMenus} setData={setSelectedMenus} />
       </S.InputWithSelectorContainer>
     </S.DualSelectorContainer>
   );
