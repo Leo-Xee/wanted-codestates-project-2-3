336731d34a0d9cbd968d89ff6ff7f5a1bfb85407
jinn2u <bsybear623@gmail.com>
2022-03-16 16:13:03 +0900

    feat: menu on/off event

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import * as S from "./style";
 
 import { BiMenu } from "react-icons/bi";
+import { settingReducer, initialSetting } from "./settingReducer";
 
 function Menu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [settingState, dispatch] = useReducer(settingReducer, initialSetting);
+
+  const handleToggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <>
-      <S.MenuButton>
+      <S.MenuButton onClick={handleToggleMenu}>
         <BiMenu />
       </S.MenuButton>
-      <S.MenuContainer>
-        <S.MenuItem>타이틀</S.MenuItem>
-        <S.MenuItem>검색</S.MenuItem>
-        <S.MenuItem>하나씩만 옮기기</S.MenuItem>
-        <S.MenuItem>아이템 크기</S.MenuItem>
-      </S.MenuContainer>
+      {menuOpen && (
+        <S.MenuContainer>
+          <S.MenuItem>타이틀</S.MenuItem>
+          <S.MenuItem>검색</S.MenuItem>
+          <S.MenuItem>하나씩만 옮기기</S.MenuItem>
+          <S.MenuItem>아이템 크기</S.MenuItem>
+        </S.MenuContainer>
+      )}
     </>
   );
 }
