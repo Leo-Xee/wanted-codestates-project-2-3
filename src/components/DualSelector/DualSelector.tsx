e1f295d9abe5b2088746ee48f44c0ca74cf1378e
Parksangwoo <psu8430@gmail.com>
2022-03-17 10:41:10 +0900

    Feat: DualSelector state 선언

--- src/components/DualSelector/DualSelector.tsx
+++ src/components/DualSelector/DualSelector.tsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 
 import SearchBar from "../SearchBar";
 import Selector from "../Selector";
 import ControlButtons from "../ControlButtons";
 
+import { emojiMenus, EmojiMenu } from "../../data/emojiMenus";
+
 function DualSelector() {
+  const [avaliable, setAvaliable] = useState<EmojiMenu[]>(emojiMenus); // 왼쪽 셀렉터 데이터 ( Available )
+  const [selected, setSelected] = useState<EmojiMenu[]>([]); // 오른쪽 셀렉터 데이터 ( Selected )
+  const [nowSelected, setNowSelected] = useState<EmojiMenu[]>([]); // 현재 선택되어있는 아이템
+
   return (
     <S.DualSelectorContainer>
       <S.InputWithSelectorContainer>
         <SearchBar />
-        <Selector />
+        <Selector nowSelected={nowSelected} setNowSelected={setNowSelected} itemList={avaliable} />
       </S.InputWithSelectorContainer>
-      <ControlButtons />
+      <ControlButtons
+        nowSelected={nowSelected}
+        setNowSelected={setNowSelected}
+        selected={selected}
+        setSelected={setSelected}
+        avaliable={avaliable}
+        setAvaliable={setAvaliable}
+      />
       <S.InputWithSelectorContainer>
         <SearchBar />
-        <Selector />
+        <Selector nowSelected={nowSelected} setNowSelected={setNowSelected} itemList={selected} />
       </S.InputWithSelectorContainer>
     </S.DualSelectorContainer>
   );
